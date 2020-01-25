2883c84a72fdc00e991ba672da6cc3ba331b3e07
Pedro Santos <pedrocarlos.ti@gmail.com>
2020-01-24 21:20:04 -0300

    Started assignment week2 | ReactJS

--- FrontEnd/week2/src/components/DishdetailComponent.js
+++ FrontEnd/week2/src/components/DishdetailComponent.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
   if (dish != null) {
@@ -51,8 +60,21 @@ const DishDetail = props => {
     return (
       <div class="container">
         <div className="row">
-          <RenderDish dish={dish} />
-          <RenderComments comments={dish.comments} />
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+
+        <div className="row">
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
--- FrontEnd/week2/src/components/MainComponent.js
+++ FrontEnd/week2/src/components/MainComponent.js
@@ -3,20 +3,52 @@ import Menu from './MenuComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import DishDetail from './DishdetailComponent';
+
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+          leader={this.state.leaders.filter(leader => leader.featured)[0]}
+        />
+      );
+    };
+
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              dish => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            comment => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
     };
 
     return (
@@ -28,6 +60,13 @@ class Main extends Component {
             exact
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/aboutus"
+            component={() => <About leaders={LEADERS} />}
           />
           <Redirect to="/home" />
         </Switch>
