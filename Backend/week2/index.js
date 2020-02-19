3a90aa57575ea97ae87380c2ad7fb162a61aa290
Pedro santos <pedrocarlos.ti@gmail.com>
2020-02-19 12:32:56 -0300

    Express Router

--- Backend/week2/index.js
+++ Backend/week2/index.js
@@ -1,56 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const http = require("http");
+const express = require("express");
+const morgan = require("morgan");
+const dishRoutes = require("./routes/dishRoutes");
 
 const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log("Request for " + req.url + " by method " + req.method);
+const app = express();
 
-  if (req.method == "GET") {
-    var fileUrl;
-    if (req.url == "/") fileUrl = "/index.html";
-    else fileUrl = req.url;
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use("/dishes", dishRoutes);
 
-    var filePath = path.resolve("./public" + fileUrl);
-    const fileExt = path.extname(filePath);
-    if (fileExt == ".html") {
-      fs.exists(filePath, exists => {
-        if (!exists) {
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html");
-          res.end(
-            "<html><body><h1>Error 404: " +
-              fileUrl +
-              " not found</h1></body></html>"
-          );
-          return;
-        }
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        fs.createReadStream(filePath).pipe(res);
-      });
-    } else {
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      res.end(
-        "<html><body><h1>Error 404: " +
-          fileUrl +
-          " not a HTML file</h1></body></html>"
-      );
-    }
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      "<html><body><h1>Error 404: " +
-        req.method +
-        " not supported</h1></body></html>"
-    );
-  }
-});
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
